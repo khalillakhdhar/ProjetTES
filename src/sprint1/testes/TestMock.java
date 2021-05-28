a209b377042a7eaab3040cf1d43329c223220643
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-05-28 14:40:54 +0100

    init

--- src/sprint1/testes/TestMock.java
+++ src/sprint1/testes/TestMock.java
@@ -2,8 +2,11 @@ package sprint1.testes;
 
 import org.junit.Before;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import sprint1.classes.Action;
+import sprint1.classes.Client;
 import sprint1.services.ActionService;
 import sprint1.services.ClientService;
 
@@ -17,6 +20,13 @@ MockitoAnnotations.initMocks(this);
 ActionService actservice;
 @InjectMocks
 ClientService clservice;
+@Mock
+Action act;
+@Mock
+Client cl;
+
+
+
 
 
 }
