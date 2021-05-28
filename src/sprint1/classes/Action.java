6b2545a841dfc666ca05cdb31f96f7c95a989283
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-05-28 10:51:04 +0100

    entities

---
+++ src/sprint1/classes/Action.java
@@ -0,0 +1,49 @@
+package sprint1.classes;
+
+public class Action {
+private int id,id_client;
+private String dateheur,type;
+private double montant;
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public int getId_client() {
+	return id_client;
+}
+public void setId_client(int id_client) {
+	this.id_client = id_client;
+}
+public String getDateheur() {
+	return dateheur;
+}
+public void setDateheur(String dateheur) {
+	this.dateheur = dateheur;
+}
+public String getType() {
+	return type;
+}
+public void setType(String type) {
+	this.type = type;
+}
+public double getMontant() {
+	return montant;
+}
+public void setMontant(double montant) {
+	this.montant = montant;
+}
+public Action(int id_client, String type, double montant) {
+	this.id_client = id_client;
+	this.type = type;
+	this.montant = montant;
+}
+public Action() {
+}
+
+
+
+
+
+}
--- src/sprint1/classes/Admin.java
+++ src/sprint1/classes/Admin.java
@@ -3,4 +3,31 @@ package sprint1.classes;
 public class Admin {
 private int id;
 private String login,mdp;
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getLogin() {
+	return login;
+}
+public void setLogin(String login) {
+	this.login = login;
+}
+public String getMdp() {
+	return mdp;
+}
+public void setMdp(String mdp) {
+	this.mdp = mdp;
+}
+public Admin() {
+}
+public Admin(String login, String mdp) {
+	this.login = login;
+	this.mdp = mdp;
+}
+
+
+
 }
