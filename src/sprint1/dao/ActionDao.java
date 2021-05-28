90381b6587b923882f43194c2d973d7005ca5672
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-05-28 14:23:00 +0100

    actionDao

---
+++ src/sprint1/dao/ActionDao.java
@@ -0,0 +1,10 @@
+package sprint1.dao;
+
+import sprint1.classes.Action;
+
+public class ActionDao {
+public void saveAction(Action action)
+{
+System.out.println(action.toString()+" ajouté avec succés");	
+}
+}
