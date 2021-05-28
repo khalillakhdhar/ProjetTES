ddc1c23f98b93bd3ae635087005da2764aa2c0ea
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-05-28 14:31:43 +0100

    ClientService

--- src/sprint1/services/ClientService.java
+++ src/sprint1/services/ClientService.java
@@ -1,5 +1,27 @@
 package sprint1.services;
 
+import sprint1.classes.Client;
+import sprint1.testes.Validation;
+
 public class ClientService {
+	Client client;
+
+	public Client getClient() {
+		return client;
+	}
+
+	public void setClient(Client client) {
+		this.client = client;
+	}
+
+	public boolean verif(Client cl) {
+		Validation valid = new Validation();
+		if ((valid.verifchaine(cl.getNom()) && (valid.verifmail(cl.getEmail())))
+				&& (valid.verifcode(cl.getCodeapplication())) && (valid.veriftel(cl.getTelephone()))) {
+			return true;
+		}
+		return false;
+
+	}
 
 }
