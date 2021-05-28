02fecc4ac551a5d94df732db3e967f2aecf45075
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-05-28 11:16:22 +0100

    notblank

--- src/sprint1/testes/TestUnit.java
+++ src/sprint1/testes/TestUnit.java
@@ -1,5 +1,30 @@
 package sprint1.testes;
 
-public class TestUnit {
+import static org.junit.Assert.assertTrue;
 
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+public class TestUnit {
+@Rule
+public ExpectedException exception=ExpectedException.none();
+public Validation validation;
+@Before
+public void beforecoding()
+{
+validation=new Validation();	
+}
+@Test
+public void testNotBlankchaine()
+{
+assertTrue(validation.verifchaine("teste"));	
+
+}
+@Test 
+public void testNotBlankNumber()
+{
+assertTrue(validation.verifchaine(String.valueOf(30)));	
+}
 }
