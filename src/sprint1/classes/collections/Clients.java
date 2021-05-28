7427fba8662143be57ce4acfb590b0779bc0461d
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-05-28 15:53:48 +0100

    clientsArraylist

--- src/sprint1/classes/collections/Clients.java
+++ src/sprint1/classes/collections/Clients.java
@@ -1,5 +1,44 @@
 package sprint1.classes.collections;
 
+import java.util.ArrayList;
+
+import sprint1.classes.Client;
+
 public class Clients {
+static ArrayList<Client> listClients=new ArrayList<Client>();
+public void addClient(Client cli)
+{
+listClients.add(cli);
+
+}
+public void afficheClients()
+{
+for(Client cl: listClients)
+{
+System.out.println(cl.toString());	
+}
+
+}
+public Client findClientById(int idclient)
+{
+	for(Client cli:listClients)
+	{
+		if(cli.getId()==idclient)
+			return cli;
+	}
+	
+	
+	return null;
+	
+}
+public static ArrayList<Client> getListClients() {
+	return listClients;
+}
+public static void setListClients(ArrayList<Client> listClients) {
+	Clients.listClients = listClients;
+}
+
+
+
 
 }
