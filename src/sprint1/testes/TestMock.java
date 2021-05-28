33d2c39db45ed5a0841d9e914e1fe712787b1006
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-05-28 14:57:33 +0100

    mocking

--- src/sprint1/testes/TestMock.java
+++ src/sprint1/testes/TestMock.java
@@ -1,12 +1,19 @@
 package sprint1.testes;
 
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
 import org.junit.Before;
+import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import sprint1.classes.Action;
 import sprint1.classes.Client;
+import sprint1.dao.ActionDao;
+import sprint1.dao.ClientDao;
 import sprint1.services.ActionService;
 import sprint1.services.ClientService;
 
@@ -21,11 +28,29 @@ ActionService actservice;
 @InjectMocks
 ClientService clservice;
 @Mock
-Action act;
+ActionDao act;
 @Mock
-Client cl;
+ClientDao cl;
 
 
+@Test
+public void addClient()
+{
+Client cli = new Client("pierre", "teste", "pierre@gmail.com", "USA", "+016667778", 282334654, 2000);
+boolean etat = clservice.saveClient(cli);
+assertTrue(etat);
+verify(cl,times(0)).saveClient(cli);
+}
+@Test
+public void addAction()
+{
+	Client cli = new Client("pierre", "teste", "pierre@gmail.com", "USA", "+016667778", 282334654, 2000);
+
+Action	ac=new Action(1, "versement",600);
+boolean state=actservice.saveAction(cli, ac);
+assertTrue(state);
+
+}
 
 
 
