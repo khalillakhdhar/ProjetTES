7d021e907d980a56a40127b017fc6a5217a4501e
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-05-28 15:58:21 +0100

    action

--- src/sprint1/classes/collections/Actions.java
+++ src/sprint1/classes/collections/Actions.java
@@ -1,5 +1,33 @@
 package sprint1.classes.collections;
 
-public class Actions {
+import java.util.ArrayList;
 
+import sprint1.classes.Action;
+
+public class Actions {
+static ArrayList<Action> listActions=new ArrayList<Action>();
+void addAction(Action act)
+
+{
+listActions.add(act);	
+
+}
+static void listerAction()
+{
+for(Action ac: listActions)
+{
+System.out.println(ac.toString());	
+
+}
+}
+public void findByClient(int idclient)
+{
+	
+for(Action ac: listActions)
+	if(ac.getId_client()==idclient)
+	{
+		System.out.println(ac.toString());
+	}
+
+}
 }
