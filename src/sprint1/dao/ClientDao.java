aac42bcdb98aa520cedb2c0ddbb17a21e6e737ae
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-05-28 14:24:13 +0100

    client

---
+++ src/sprint1/dao/ClientDao.java
@@ -0,0 +1,11 @@
+package sprint1.dao;
+
+import sprint1.classes.Client;
+
+public class ClientDao {
+public void saveClient(Client client)
+{
+System.out.println(client.toString()+ " ajouté avec succés");	
+
+}
+}
