5369b6e7d452c07dce370176a73f8303fa2b33bb
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-05-28 10:54:52 +0100

    testing

---
+++ src/sprint1/testes/Validation.java
@@ -0,0 +1,14 @@
+package sprint1.testes;
+
+public class Validation {
+public boolean validaccess(String accessdata)
+{
+if(accessdata.length()>=12)
+{
+return true;	
+}
+else 
+	return false;
+
+}
+}
