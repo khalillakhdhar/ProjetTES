f0c043267522ef16d5a962b200c3e9ddce60c7f5
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-05-31 11:22:46 +0100

    Update ActionDao.java

--- src/sprint1/dao/ActionDao.java
+++ src/sprint1/dao/ActionDao.java
@@ -1,10 +1,27 @@
 package sprint1.dao;
 
+import java.sql.SQLException;
+
 import sprint1.classes.Action;
 
 public class ActionDao {
+	Action action;
 public void saveAction(Action action)
 {
 System.out.println(action.toString()+" ajouté avec succés");	
+}
+public void addAction()
+{
+action=new Action(1, "retrait", 20);	
+try {
+	// TODO add your handling code here:
+	            Connexion c = new Connexion();
+	            java.sql.PreparedStatement statement = c.conn.prepareStatement("INSERT INTO `actions`(  `montant`, `type`, `id_client`) VALUES ('" + action.getId_client()+ "','" + action.getMontant() + "','" + action.getType() + "')");
+	            statement.executeUpdate();
+	            System.out.println("ajouté avec succés");
+	        } catch (SQLException ex) {
+	                        System.out.println(ex);
+
+	        }	
 }
 }
